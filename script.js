d9101551505ec69d0e8f613253b7c7625412d18a
rodriguessantosmaria0403-dotcom <rodrigues.santos.maria0403@escola.pr.gov.br>
2026-06-09 11:33:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Efeito de mudar o fundo do menu ao rolar a página
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.style.backgroundColor = '#ffffff';
-        header.style.boxShadow = '0 4px 12px rgba(0,0,0,0.15)';
-    } else {
-        header.style.boxShadow = '0 2px 10px rgba(0,0,0,0.1)';
-    }
-});
+
